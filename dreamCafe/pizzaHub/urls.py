cf62a159b0dd858b020a7446e28ae3e113e9d2fb
Rkverma1996 <sweetsanjeevkumar96@gmail.com>
2020-08-25 16:47:34 +0530

    Implement model for pizza info and user login also

--- dreamCafe/pizzaHub/urls.py
+++ dreamCafe/pizzaHub/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-
-from .views import homePageView
+from django.conf import settings
+from django.conf.urls.static import static
+from .views import homePageView, loginView, logoutView
 
 urlpatterns = [
-    path('', homePageView, name='home')
-]
+    path('', homePageView, name='home'),
+    path('login/', loginView, name='login'),
+    path('logout/', logoutView, name='logout')
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- dreamCafe/pizzaHub/views.py
+++ dreamCafe/pizzaHub/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import TemplateView
+from django.contrib.auth.models import User
+from django.contrib.auth import login, logout, authenticate
+from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+def loginView(request):
+  if request.method == 'POST':
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(username=username, password=password)
+    if user is not None:
+      login(request, user)
+      return redirect('home')
+    else:
+      return render(request, 'login.html')
+  return render(request, 'login.html')
+
+def logoutView(request):
+  logout(request)
+  return redirect('logout')
+
+@login_required(login_url='login/')
 def homePageView(request):
   return render(request, 'home.html', {'message': 'Login fail.'})
